375bd1731634d7b4bf2b89d4eff62c1f65dadded
AndreasHelboJensen <andheljensen@gmail.com>
2026-09-14 14:45:55 +0200

    version 2

--- src/main.py
+++ src/main.py
@@ -7,11 +7,11 @@ MOTOR_CAPACITY = 12
 ### YOUR CODE HERE
 
 motor_count = int(input("How many motors are carrying the packages?"))
-print(motor_count)
+
 total_package_weight = int(input("How many kg of packages do we expect?"))
-print(total_package_weight)
+
 
 if total_package_weight <= motor_count*MOTOR_CAPACITY:
     print("Yes! The conveyor belt can carry the packages.")
 else:
-    print("No. The conveyor belt cannot carry the packages.")
+    print("No. The conveyor belt can4not carry the packages.")
